f38e790e48771c1716ffb65416580b254d780eec
honorabrutroll <edward.yang6771@gmail.com>
2015-08-06 16:45:17 -0500

    started useer input project

---
+++ user_input/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+
+
+fn main() {
+    let mut guess = String::new();
+    println!("Enter something.");
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Readline failed.");
+
+    println!("You entered \"{}\".", guess);
+}
