b1c5affd37171536de3f26d7e0c0bed4c3138cc3
honorabrutroll <edward.yang6771@gmail.com>
2015-08-06 15:37:54 -0500

    Implemented var_example

---
+++ var_example/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let var1: i16;
+    let var2: i16;
+
+    var1 = 1024;
+
+    println!("The value of var1 is: {}.", var1);
+
+    var2 = var1 / 2;
+    
+    print!("The value of var2 is var1 / 2, or {}.", var2);
+}
