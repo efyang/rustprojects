35309458139a79f67ba700fbf77ef39d8aa842f7
honorabrutroll <edward.yang6771@gmail.com>
2015-08-06 22:36:22 -0500

    Added description of problem

--- betting/src/main.rs
+++ betting/src/main.rs
@@ -1,3 +1,10 @@
+/*Here is a simple game involving coin tosses: A player is given a starting amount of cash (perhaps $50) and repeatedly bets an amount of money (perhaps $1 or $2) on the outcome of a fair coin toss. Each time the coin is tossed, the player decides how much money to bet on that toss. The player will continue to play the game until one of three events happens:
+
+    He reaches a predefined goal ($250) and walks away feeling happy (Win).
+    He loses all of his cash ($0) and walks away feeling sad (Lose).
+    He plays too many rounds (20000) and walks away feeling bored (Lose). (For the purposes of this assignment, we will always consider the failure to reach the target amount in the stipulated number of bets as a loss.)*/
+extern crate rand;
+
 fn main() {
-    println!("Hello, world!");
+    
 }
