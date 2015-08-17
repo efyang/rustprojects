9ab5282029d6199bdf8f3e497e7f5f289092e0ce
honorabrutroll <edward.yang6771@gmail.com>
2015-08-17 00:38:00 -0500

    random updates

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -1,3 +1,19 @@
+struct BinaryTree {
+    data: std::Vec,
+}
+
+impl BinaryTree{
+    fn make_binary(&mut self){
+        if self.len() > 1 {
+            if self.get(0) != 0{
+                self.insert(0,0)
+            }
+        }
+    }
+
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
