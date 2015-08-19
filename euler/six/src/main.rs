6577d08b19299bcf0da8d6f453eb6b3c14d5990f
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 14:31:43 -0500

    Finished six

--- euler/six/src/main.rs
+++ euler/six/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut square_sum: i64 = 0;
+    let mut sum: i64 = 0;
+    for x in 1..101 as i64 {
+        square_sum += x.pow(2);
+        sum += x;
+    }
+
+    println!("Difference: {}", sum.pow(2) - square_sum);
 }
