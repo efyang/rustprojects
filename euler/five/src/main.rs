f007532d4b19d8e82c91307a32d89ab6308184d8
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 14:17:33 -0500

    Finished 5

--- euler/five/src/main.rs
+++ euler/five/src/main.rs
@@ -1,3 +1,19 @@
+fn isdivisible(n: i64) -> bool {
+    for x in 2..21 as i64 {
+        if n % x != 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut current: i64 = 2520;
+    loop {
+        if isdivisible(current) {
+            println!("{}", current);
+            break;
+        }
+        current += 20;
+    }
 }
