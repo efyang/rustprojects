59258164a3941c25b020fb49f5367a37f6da2bd3
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 11:01:38 -0500

    Finished problem 2

--- euler/two/src/main.rs
+++ euler/two/src/main.rs
@@ -1,3 +1,27 @@
+fn fib_list_to_max(max: i64) -> Vec<i64>{
+    let mut fib_list: Vec<i64> = vec![1,2];
+    let mut index;
+    let mut next_fib: i64;
+    loop{
+        index = fib_list.len() - 1;
+        next_fib = fib_list[index] + fib_list[index - 1];
+        if next_fib >= max{
+            break
+        }
+        else{
+            fib_list.push(next_fib);
+        }
+    }
+    return fib_list;
+}
+
+fn even_sum(values: Vec<i64>) -> i64{
+    return values
+        .iter()
+        .filter(|&x| x%2 == 0 )
+        .fold(0i64, |acc, &item| acc + item);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",even_sum(fib_list_to_max(4000000)));
 }
