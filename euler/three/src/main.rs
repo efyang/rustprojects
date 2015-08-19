fcb311e906c4e2b47b638c3873d1f67baff823a8
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 13:37:13 -0500

    finished problem 3

--- euler/three/src/main.rs
+++ euler/three/src/main.rs
@@ -1,3 +1,10 @@
+#![feature(step_by)]
+#![feature(append)]
+mod prime;
+use prime::{factorize,is_prime};
+
 fn main() {
-    println!("Hello, world!");
+    let mut factors: Vec<u64> = prime::factorize(600851475143 as u64);
+    factors.sort();
+    println!("Largest prime factor of 600851475143 is: {}", factors.last().unwrap());
 }
---
+++ euler/three/src/prime.rs
@@ -0,0 +1,41 @@
+
+
+fn even(n : u64) -> bool{
+    return n % 2 == 0;
+}
+
+pub fn is_prime(n: u64) -> bool{
+    if even(n) {
+    return false;
+    }
+    else{
+        for x in (3..(n as f64).sqrt() as u64 + 1).step_by(2) {
+            if n % x == 0{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+pub fn factorize(n: u64) -> Vec<u64> {
+    let mut factors: Vec<u64> = Vec::new();
+    if even(n) {
+        factors.push(2);
+        factors.append(&mut factorize(n/2));
+    }
+    else{
+        for x in (3..(n as f64).sqrt() as u64 + 1).step_by(2) {
+            if n % x == 0{
+                factors.append(&mut factorize(x));
+                factors.append(&mut factorize(n/x));
+            }
+        }       
+    }
+    if factors.is_empty() {
+        return vec![n];
+    }
+    else{
+        return factors;
+    }
+}
