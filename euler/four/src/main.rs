ac234610bcd6a509e8be62407a44d8529d10d33f
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 14:09:30 -0500

    Finished 4

--- euler/four/src/main.rs
+++ euler/four/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let mut string: String;
+    let mut rev_string: String;
+    let mut palindromes: Vec<i64> = Vec::new();
+    let mut value: i64;
+    for x in (100..1000).rev() {
+        for y in (100..1000).rev() {
+            value = x*y;
+            string = value.to_string();
+            rev_string = string
+                .chars()
+                .rev()
+                .collect();
+            //println!("{} {}", string, rev_string);
+            if string == rev_string {
+                palindromes.push(value);
+                break;
+            }
+        }
+    }
+    palindromes.sort();
+    println!("{}", palindromes.last().unwrap());
 }
