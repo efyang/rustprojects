4ca804d230141abd6718f8312bf0a250d6c0a25b
honorabrutroll <edward.yang6771@gmail.com>
2015-08-20 13:07:20 -0500

    finished 10 (very slow)

--- euler/ten_parallel/src/main.rs
+++ euler/ten_parallel/src/main.rs
@@ -2,13 +2,14 @@
 #![feature(iter_arith)]
 #![feature(append)]
 use std::thread;
-use std::sync::{Arc, Mutex};
+//use std::sync::{Arc, Mutex};
+use std::sync::mpsc::channel;
 extern crate num_cpus;
 
 fn get_indices(thread_n: u64, total_proc: u64, list_length: u64) -> (u64, u64) {
     let start: u64 = thread_n * list_length/total_proc;
-    let end: u64 = ((thread_n + 1) * list_length/total_proc) - 1;
-    return (start, end) 
+    let end: u64 = (thread_n + 1) * list_length/total_proc;
+    return (start, end);
 } 
 
 fn vector_cut(vec: Vec<u64>, start: u64, end: u64) -> Vec<u64> {
@@ -21,48 +22,47 @@ fn vector_cut(vec: Vec<u64>, start: u64, end: u64) -> Vec<u64> {
 
 fn sieve(limit: u64) -> Vec<u64> {
     let max_threads: u64 = num_cpus::get() as u64;
-    let mut list: Vec<u64> = (3..limit).step_by(2).filter(|&x| x % 3 != 0).collect();
-    let mut newlist = Arc::new(Mutex::new(list.clone()));
+    let mut list: Vec<u64> = (3..(limit + 1)).step_by(2).collect();
     let mut primes: Vec<u64> = vec![2,3];
     let mut prime: u64 = 3;
+    let mut list_len = list.len() as u64;
 
-    let mut list_len = list.len();
-    while list_len >= max_threads as usize {
-        for num in 0..max_threads {
-            let newlist = newlist.clone();
+    while list_len >= max_threads {
+        let mut recvlist: Vec<Vec<u64>> = Vec::new();
+        let mut list_accum: Vec<u64> = Vec::new();
+        let (tx,rx) = channel();
+        //let mut list_accum = Arc::new(Mutex::new(Vec::<u64>::new()));
+        for t_num in 0..max_threads {
+            let tx = tx.clone();
+            let list_len = list_len.clone();
             let list = list.clone();
-            
-            //spawn threads and partition off pieces of the main list (index and clone)
+            let (start,mut end) = get_indices(t_num, max_threads, list_len);
+            if t_num == max_threads - 1 {
+                end = list_len - 1;
+            }
+            let list_piece: Vec<u64> = vector_cut(list, start, end);
             thread::spawn(move || {
-                let (start, end) = get_indices(num, max_threads, list_len as u64);
-                let mut mylist: Vec<u64> = vector_cut(list,start,end);
-                println!("{:?}",mylist.first());
-                newlist.lock().unwrap().append(&mut mylist
-                                               .into_iter()
-                                               .filter(|&n| n % prime != 0)
-                                               .collect()); 
+                let newlist: Vec<u64> = list_piece.into_iter().filter(|&n| n % prime != 0).collect();
+                tx.send(newlist.clone()).unwrap();
             });
         }
-        list.clone_from(&newlist.lock().unwrap().clone());
-        list.sort();
+        for _ in 0..max_threads {
+            recvlist.push(rx.recv().unwrap());
+        }
+        recvlist.sort();
+        for item in recvlist {
+            list_accum.append(&mut item.clone());
+        }
+        
+        list.clone_from(&list_accum);
+        list_len = list.len() as u64;
         prime = list.remove(0);
         primes.push(prime.clone());
-        list_len = list.len();
     }
-
-    //while !list.is_empty() {
-        //list = list.into_iter().filter(|&item| item % prime != 0).collect();
-        //prime = list.pop().unwrap();
-        //primes.push(prime.clone());
-    //} 
+    primes.append(&mut list.clone());
     return primes;
 }
 
 fn main() {
     println!("Sum: {}", sieve(2000000 as u64).iter().sum::<u64>());
-    //let mut primes: Vec<u64> = vec![2,3,4,5,6,7];
-    //for x in 0..8 {
-        //println!("{:?}",get_indices(x,8,8));
-    //}
-    //println!("{:?}",vector_cut(primes,0,2));
 }
