5b3aebb4ff544527a33f63717abe41e00ba8aae7
honorabrutroll <edward.yang6771@gmail.com>
2015-08-19 19:30:05 -0500

    Finished divider function

--- euler/ten_parallel/src/main.rs
+++ euler/ten_parallel/src/main.rs
@@ -3,8 +3,16 @@
 use std::thread;
 use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex};
+extern crate num_cpus;
+
+fn get_indices(thread_n: u64, total_proc: u64, list_length: u64) -> (u64, u64) {
+    let start: u64 = thread_n * list_length/total_proc;
+    let end: u64 = ((thread_n + 1) * list_length/total_proc) - 1;
+    return (start, end) 
+} 
 
 fn sieve(limit: u64) -> Vec<u64> {
+    let max_threads: u64 = num_cpus::get() as u64;
     let mut list: Vec<u64> = (3..limit).step_by(2).collect();
     list.reverse();
     let mut primes: Vec<u64> = vec![2,3];
@@ -19,5 +27,5 @@ fn sieve(limit: u64) -> Vec<u64> {
 }
 
 fn main() {
-    println!("Sum: {}", sieve(2000000 as u64).iter().sum::<u64>());
+    //println!("Sum: {}", sieve(2000000 as u64).iter().sum::<u64>());
 }
