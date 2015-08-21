b29836a76997351c5cd0da72246c83205f190735
honorabrutroll <edward.yang6771@gmail.com>
2015-08-20 21:39:11 -0500

    Working on p13 file handling

--- euler/p13/src/main.rs
+++ euler/p13/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = try!(File::open("numbers.txt")); 
+    let mut data: Vec<u8> = Vec::new();
+    let mut data2 = String::new();
+    try!(f.read_to_string(&mut data2));
+    println!("{}",data2);
+    //for chunk in data.iter().chunks(52) {
+        //println!("{:?}",chunk);
+    //}
 }
