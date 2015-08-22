49898e8e72ee063e214754e38180c6487980e850
honorabrutroll <edward.yang6771@gmail.com>
2015-08-21 23:17:43 -0500

    Finished 15

---
+++ euler/p15/src/main.rs
@@ -0,0 +1,19 @@
+extern crate num;
+use num::bigint::{BigUint, ToBigUint};
+fn factorial(n: BigUint) -> BigUint {
+    let one = 1.to_biguint().unwrap();
+    if n != one {
+        return n.clone() * factorial(n - one);
+    }
+    else {
+        return n;
+    }
+}
+
+fn combination(n: BigUint, r: BigUint) -> BigUint {
+    return factorial(n.clone()) / (factorial(r.clone()) * factorial(n - r));
+}
+
+fn main() {
+    println!("{}", combination(40u64.to_biguint().unwrap(),20u64.to_biguint().unwrap()));
+}
