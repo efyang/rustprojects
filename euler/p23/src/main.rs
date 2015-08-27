f70d8ada55f0f240ca86d4e249dbce2e291cf7c4
honorabrutroll <edward.yang6771@gmail.com>
2015-08-27 17:58:37 -0500

    Finished p23

--- euler/p23/src/main.rs
+++ euler/p23/src/main.rs
@@ -8,19 +8,31 @@ fn is_abundant_number(n: &u64) -> bool {
 }
 
 fn get_abundants(limit: &u64) -> Vec<u64> {
-    if limit > &28123u64 {
-        (1..28124u64).filter(|x| is_abundant_number(&x)).collect::<Vec<u64>>()
-    } 
-    else {
-        (1..*limit).filter(|x| is_abundant_number(&x)).collect::<Vec<u64>>()
-    }
+    (1..*limit).filter(|x| is_abundant_number(&x)).collect::<Vec<u64>>()
 }
 
 fn get_all_abundants() -> Vec<u64> {
-    get_abundants(&28112u64)
+    get_abundants(&28124u64)
+}
+
+fn get_abundant_sums() -> Vec<u64> {
+    let abundants: Vec<u64> = get_all_abundants();
+    let mut abundant_sums: Vec<u64> = Vec::new();
+    for x in abundants.iter() {
+        for y in abundants.iter() {
+            if x + y < 28124 {
+                abundant_sums.push(x + y);
+            }
+        }
+    }
+    abundant_sums 
+}
+
+fn get_non_abundant_sum() -> u64 {
+    let abundant_sums = get_abundant_sums();
+    (1..28124u64).filter(|x| !abundant_sums.contains(&x)).sum::<u64>() 
 }
 
 fn main() {
-    let abundants: Vec<u64> = get_all_abundants();
-    println!("{:?}", abundants);
+    println!("Sum of all non-abundants: {:?}", get_non_abundant_sum());
 }
