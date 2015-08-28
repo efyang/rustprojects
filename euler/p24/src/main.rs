586e0694c3227b03296576735b3f4e71f1fa846b
honorabrutroll <edward.yang6771@gmail.com>
2015-08-28 08:30:02 -0500

    Minor additions to p24

--- euler/p24/src/main.rs
+++ euler/p24/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", get_last_lex_permutation(1000000, vec![1,2,3,4,5,6,7,8,9u64]));
 }
 
-fn get_lex_permutations(items: Vec<u64>) -> Vec<String> {
+fn get_last_lex_permutation(limit: u64, items: Vec<u64>) -> Vec<u64> {
     
 }
