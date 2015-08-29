627ecea293f889d2af9f172a19ef93018c1a5899
honorabrutroll <edward.yang6771@gmail.com>
2015-08-28 19:56:54 -0500

    Finished 24

--- euler/p24/src/main.rs
+++ euler/p24/src/main.rs
@@ -1,7 +1,54 @@
+#![feature(split_off)]
+#![feature(append)]
+
 fn main() {
-    println!("{:?}", get_last_lex_permutation(1000000, vec![1,2,3,4,5,6,7,8,9u64]));
+    println!("{:?}", pandita_to_limit(&1000000u64, &vec![0,1,2,3,4,5,6,7,8,9u64]));
 }
 
-fn get_last_lex_permutation(limit: u64, items: Vec<u64>) -> Vec<u64> {
-    
+fn pandita_to_limit(limit: &u64, items: &Vec<u64>) -> Vec<u64> {
+    let mut sequence: Vec<u64> = items.clone();
+    let mut permutations: u64 = 1;
+    let seq_length: usize = sequence.len();
+    let mut k: usize;
+    let mut i: usize = 0;
+    let mut index: isize;
+    if seq_length <= 1 {
+        return sequence;
+    }
+    sequence.sort();
+
+    while &permutations < limit {
+        k = seq_length;
+        index = seq_length as isize - 2;
+        while index >= 0 {
+            if sequence[index as usize] < sequence[index as usize + 1] {
+                k = index as usize;
+                break;
+            }
+            index -= 1;
+        }
+        if k == seq_length {
+            return sequence;
+        }
+        index = seq_length as isize - 1;
+        while index >= 0 {
+            if sequence[index as usize] > sequence[k] {
+                i = index as usize;
+                break;
+            } 
+            index -= 1;
+        }
+        sequence.swap(k, i);
+        let mut suffix: Vec<u64> = sequence.split_off(k + 1)
+            .iter()
+            .map(|x| *x)
+            .rev()
+            .collect::<Vec<u64>>();
+        sequence.append(&mut suffix);
+        //println!("{:?}", permutations);
+        permutations += 1;
+    }
+    sequence
 }
+
+
