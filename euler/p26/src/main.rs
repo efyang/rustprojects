2cdf49476584dd54f48fe6c25931a1571a499f37
honorabrutroll <edward.yang6771@gmail.com>
2015-08-30 16:38:08 -0500

    Finished 26

--- euler/p26/src/main.rs
+++ euler/p26/src/main.rs
@@ -1,7 +1,23 @@
+#![feature(iter_cmp)]
+
 fn main() {
-    for d in 1..200 {
-        println!("1/{} : {}", d, 1f64 / (d as f64));
+    println!("longest recurring cycle : 1/{}", (2..1000usize).max_by(|x| longest_recurring_cycle(1, *x)).unwrap() );
+}
+
+fn longest_recurring_cycle(n: usize, d: usize) -> usize {
+    let rem = n % d;
+    let mut rems: Vec<usize> = vec![rem];
+    let mut value: usize = (rem * 10) % d;
+    let mut length: usize = 0;
+    loop {
+        value = (rem * value * 10) % d;
+        if rem == 0 || rems.contains(&value) {
+            break;
+        }
+        length += 1;
+        rems.push(value);
     }
+    length
 }
 
 
