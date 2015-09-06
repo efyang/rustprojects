9665315fa7805b85b17959f1e5b9c7f316539eb4
Edward Yang <edward.yang6771@gmail.com>
2015-09-05 20:11:05 -0500

    Work on main loop

--- conway/src/main.rs
+++ conway/src/main.rs
@@ -1,5 +1,26 @@
 extern crate rustty;
+use rustty::{Terminal, Cell, Style, Color, Event};
+use std::thread::sleep_ms;
 
 fn main() {
-    println!("Hello, world!");
+    let mut term = Terminal::new().unwrap();
+    let white = Style::with_color(Color::White);
+    let black = Style::with_color(Color::Black);
+
+    loop {
+        term.clear_with_styles(white, white).unwrap();
+        let evt = term.get_event(0).unwrap();
+        if evt.is_some() {
+            match evt.unwrap() {
+                Event::Key('s') => break, //break
+                Event::Key(_)   => {},    //do nothing
+            }
+        }
+        //main code
+        
+
+
+        sleep_ms(10u32);
+        term.swap_buffers().unwrap();
+    }
 }
