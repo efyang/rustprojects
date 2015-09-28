fb1e4afcce71bc144281417dac78a7cf10d98de0
honorabrutroll <edward.yang6771@gmail.com>
2015-09-27 22:34:15 -0500

    More work on iterate function

--- usaco/2015/bronze/open/trapped/src/main.rs
+++ usaco/2015/bronze/open/trapped/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(result_expect)]
+#![feature(iter_arith)]
 #![feature(convert)]
 #![feature(slice_splits)]
 use std::fs::File;
@@ -8,7 +9,6 @@ fn main() {
     //(size, position)
     let data: Vec<(usize, usize)> = read_data();
     println!("{:?}", iterate(&data));
-    println!("{:?}", data);
 }
 
 fn read_data() -> Vec<(usize, usize)> {
@@ -37,16 +37,6 @@ fn write_data(data: &usize) {
 }
 
 fn iterate(data: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
-    fn get_distances(data: &Vec<(usize, usize)>) -> Vec<usize> {
-        //gets distances between bales
-        let init = data.split_last().unwrap().1;
-        let tail = data.split_first().unwrap().1;
-        let distances: Vec<usize> = init.iter().zip(tail.iter())
-            .map(|datapair| (datapair.1).1 - (datapair.0).1)
-            .collect::<Vec<usize>>();
-        distances
-    }
-
     fn remove_bale_posns(data: &Vec<(usize, usize)>, posns: &Vec<usize>) -> Vec<(usize, usize)> {
         //removes the bales that match with the posns
         data.to_owned()
@@ -58,14 +48,40 @@ fn iterate(data: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
     let distances = get_distances(data);
     let mut dataclone = data.to_owned();
     dataclone.reserve(0);
+    let mut to_remove: Vec<usize> = Vec::new();
 
-    for bale in dataclone.iter() {
-        println!("{:?}", bale);
+    for (bale, distance) in init(&dataclone).iter().zip(distances.iter()) {
+        if distance > &bale.0 {
+            to_remove.push(bale.1);
+        }
     }
-    //for bale in data.iter().rev() {
-        
-    //}
+    dataclone = remove_bale_posns(&dataclone, &to_remove);
+    to_remove.clear();
+    for (bale, distance) in tail(&dataclone).iter().zip(distances.iter()) {
+        if distance > &bale.0 {
+            to_remove.push(bale.1);
+        }
+    }
+    dataclone = remove_bale_posns(&dataclone, &to_remove);
+    
     dataclone
 }
 
+fn init(data: &Vec<(usize, usize)>) -> &[(usize, usize)] {
+    data.split_last().unwrap().1
+}
+
+fn tail(data: &Vec<(usize, usize)>) -> &[(usize, usize)] {
+    data.split_first().unwrap().1
+}
+
+fn get_distances(data: &Vec<(usize, usize)>) -> Vec<usize> {
+    //gets distances between bales
+    let init = init(&data);
+    let tail = tail(&data);
+    let distances: Vec<usize> = init.iter().zip(tail.iter())
+        .map(|datapair| (datapair.1).1 - (datapair.0).1)
+        .collect::<Vec<usize>>();
+    distances
+}
 
