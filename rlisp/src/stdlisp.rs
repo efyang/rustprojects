78f80b1d552de40fc34aa6380be6433bcf285558
Edward Yang <edward.yang6771@gmail.com>
2015-10-14 12:01:05 -0500

    Make it compile

--- rlisp/src/stdlisp.rs
+++ rlisp/src/stdlisp.rs
@@ -1,15 +1,34 @@
 use data::*;
 
 pub static BASE_FUNCTIONS: &'static [Function<'static>] = &[
-    Function {name: "+", procedure: &(add as fn(Vec<Object>, &mut Env) -> Object)}
-    //Function {name: "-", procedure: &(subtract as fn(Vec<Object>) -> Object)}
+    Function {name: "+", procedure: &(add as fn(Vec<Object>, &mut Env) -> Object)},
+    //Function {name: "-", procedure: &(subtract as fn(Vec<Object>, &mut Env) -> Object)},
+    Function {name: "list", procedure: &(list as fn(Vec<Object>, &mut Env) -> Object)}
 ];
 
 fn add(args: Vec<Object>, env: &mut Env) -> Object {
-    unimplemented!();
+    let numbers = args.iter()
+        .map(|x| get_number(x))
+        .collect::<Vec<Number>>();
+
+    Object::Number(numbers.first().unwrap().clone())
 }
 
-fn subtract(args: Vec<Object>, env: &mut Env) -> Object {
-    unimplemented!();
+//fn subtract(args: Vec<Object>, env: &mut Env) -> Object {
+    //for object in args.iter() {
+    
+    //}
+//}
+
+fn get_number(object: &Object) -> Number {
+    if let &Object::Number(ref n) = object {
+        n.clone()
+    } else {
+        panic!("Object {:?} is not a number.", object);
+    }
+}
+
+fn list(args: Vec<Object>, env: &mut Env) -> Object {
+    Object::List(Box::new(args))
 }
 
