6a4842a3e67092c20f2ef41224f8a4eb899dc753
Edward Yang <edward.yang6771@gmail.com>
2015-10-14 18:52:40 -0500

    Make repl

--- rlisp/src/main.rs
+++ rlisp/src/main.rs
@@ -12,12 +12,18 @@ use clap::{Arg, App};
 use data::*;
 use parser::*;
 use eval::*;
+use std::io::{self, Write};
+
+const NAME: &'static str = "rlisp";
+const VERSION: &'static str = "1.0";
+const AUTHOR: &'static str = "Edward Yang <edward.yang6771@gmail.com>";
+const INFO: &'static str = "A basic lisp interpreter in rust.";
 
 fn main() {
-    let matches = App::new("rlisp")
-        .version("1.0")
-        .author("Edward Yang <edward.yang6771@gmail.com>")
-        .about("Basic lisp interpreter in rust")
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .author(AUTHOR)
+        .about(INFO)
         .args_from_usage(
             "-i --interactive=[INTERACTIVE] 'optional - Enables interactive repl - enabled if no file specified'
             -f --file=[FILE] 'optional - specifies a file to load'")
@@ -28,11 +34,34 @@ fn main() {
     //} else {
     
     //}
-    let parsed = parse(&"(cons 1 (list 1 2))".to_string());
-    let mut stdenv = Env::new();
-    println!("{:?}", parsed.eval(&mut stdenv));
+    //let parsed = parse(&"(cons 1 (list 1 2))".to_string());
+    //let mut stdenv = Env::new();
+    //println!("{:?}", parsed.eval(&mut stdenv));
+    repl(None);
 }
 
-fn repl(file: &str) {
+fn repl(file: Option<&str>) {
+    println!("\r\nStarting REPL for {name} {version}
+{author}
+{info}\n", 
+name = NAME,
+version = VERSION,
+author = AUTHOR,
+info = INFO);
+    let mut reader = io::stdin();
+    let stdout = io::stdout();
+    let mut writer = stdout.lock();
+    let mut stdenv = Env::new();
+    if let Some(filename) = file {
+        println!("{:?}", parse_file(filename).eval(&mut stdenv));
+    }
+    loop {
+        writer.write(b">>> ").expect("Failed to write line.");
+        writer.flush().expect("Failed to flush stdout.");
+        let mut input = String::new();
+        reader.read_line(&mut input).expect("Failed to read line.");
+        println!("{:?}", parse(&input).eval(&mut stdenv));
+    }
+
     unimplemented!();
 }
